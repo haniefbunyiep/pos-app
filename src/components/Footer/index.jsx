38d05c5bfd82d802289dfbf7482547389956aa13
haniefbunyiep <haniefburhan17@gmail.com>
2024-03-16 04:51:23 +0700

    feat : footer responsive

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,14 +1,51 @@
 export default function Footer() {
   return (
     <>
-      {/* Sm */}
-      <div className="md:hidden">
+      {/* Mobile */}
+      <div className="sm:hidden md:hidden">
         <footer className="flex h-[70px] flex-col items-center justify-center gap-1 bg-alabaster ">
           <span className="flex items-center justify-center">
             ©️2022 TOMORO COFFEE. All rights reserved.
           </span>
           <span className="flex items-center justify-center">
-            Contact us hitomoro@tomoro-coffee.com
+            Contact us&nbsp;
+            <a
+              href="mailto:hitomoro@tomoro-coffee.com"
+              className="text-burnorange"
+            >
+              hitomoro@tomoro-coffee.com
+            </a>
+          </span>
+        </footer>
+      </div>
+      {/* Sm */}
+      <div className="mobile:hidden md:hidden">
+        <footer className="flex h-[70px] flex-col items-center justify-center gap-1 bg-alabaster ">
+          <span className="flex items-center justify-center">
+            ©️2022 TOMORO COFFEE. All rights reserved.
+          </span>
+          <span className="flex items-center justify-center">
+            Contact us&nbsp;
+            <a
+              href="mailto:hitomoro@tomoro-coffee.com"
+              className="text-burnorange"
+            >
+              hitomoro@tomoro-coffee.com
+            </a>
+          </span>
+        </footer>
+      </div>
+      {/* Default */}
+      <div className="mobile:hidden sm:hidden md:block">
+        <footer className="flex h-[70px] items-center justify-center bg-alabaster">
+          <span>
+            ©️2022 TOMORO COFFEE. All rights reserved. Contact us {""}
+            <a
+              href="mailto:hitomoro@tomoro-coffee.com"
+              className="text-burnorange"
+            >
+              hitomoro@tomoro-coffee.com
+            </a>
           </span>
         </footer>
       </div>
